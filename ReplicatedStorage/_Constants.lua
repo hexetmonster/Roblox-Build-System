5b6a4038d04c30b7e62b618bae9c6ed75f67dc44
hexetmonster <57297409+hexetmonster@users.noreply.github.com>
2026-07-22 08:37:54 -0500

    Create _Constants.lua

---
+++ ReplicatedStorage/_Constants.lua
@@ -0,0 +1,13 @@
+--!strict
+return {
+	SavingEnabled = false; --# TBD
+	DatastoreName = "BuildSystem_Data_V1";
+	MaxUndoSteps = 50;
+	UseBase = true;
+	DoCollisions = true;
+	GridSnap = 1;
+	SegmentAngleSnap = 15;
+	RotationSnap = 45;
+	MaxScale = 10;
+	MinScale = 0.5;
+}
