f2c6d80522b4b4fb263828c85ff83851d31e5656
hexetmonster <57297409+hexetmonster@users.noreply.github.com>
2026-07-22 08:38:55 -0500

    Create _Types.lua

---
+++ ReplicatedStorage/_Types.lua
@@ -0,0 +1,6 @@
+--!strict
+local _Types = {}
+export type Action = {action:string, instance:Instance}
+export type BuildServer = typeof(setmetatable({} :: {PlayerHistory:{[Player]:{stack:{Action}, pointer:number}}}, BuildServer))
+export type BuildClient = typeof(setmetatable({} :: {Player:Player, GhostModel:Model?, HitboxVisual:SelectionBox?, IsRemoving:boolean, CurrentRotation:number, CurrentScale:number, _renderConnection:RBXScriptConnection?, PointA:Vector3?, PlacementMode:string, OriginalSize:Vector3?}, BuildClient))
+return _Types
