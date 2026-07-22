7d9bb632dd3ae6f1159e334daca48ea7cf1d2a9d
hexetmonster <57297409+hexetmonster@users.noreply.github.com>
2026-07-22 11:25:49 -0500

    Update _Types.lua

--- ReplicatedStorage/_Types.lua
+++ ReplicatedStorage/_Types.lua
@@ -1,7 +1,51 @@
 --!strict
--- == ignore == --
+-- == normal users plz ignore == --
 local _Types = {}
-export type Action = {action:string, instance:Instance}
-export type BuildServer = typeof(setmetatable({} :: {PlayerHistory:{[Player]:{stack:{Action}, pointer:number}}}, BuildServer))
-export type BuildClient = typeof(setmetatable({} :: {Player:Player, GhostModel:Model?, HitboxVisual:SelectionBox?, IsRemoving:boolean, CurrentRotation:number, CurrentScale:number, _renderConnection:RBXScriptConnection?, PointA:Vector3?, PlacementMode:string, OriginalSize:Vector3?}, BuildClient))
+
+export type Action = {
+	action:"Place" | "Remove",
+	instance:Instance,
+}
+
+export type PlayerHistory = {
+	stack:{Action},
+	pointer:number,
+}
+
+export type BuildServer = {
+	PlayerHistory:{[Player]:PlayerHistory},
+	Place:(self:BuildServer, player:Player, objectName:any, cf:any, scaleOrSize:any, connectedTargets:any, requestedMode:any) -> boolean,
+	Remove:(self:BuildServer, player:Player, target:any) -> boolean,
+	Undo:(self:BuildServer, player:Player) -> boolean,
+	Redo:(self:BuildServer, player:Player) -> boolean,
+}
+
+export type BuildClient = {
+	Player:Player,
+	GhostModel:Model?,
+	HitboxVisual:SelectionBox?,
+	IsRemoving:boolean,
+	CanPlace:boolean,
+	CurrentRotation:number,
+	CurrentScale:number,
+	_renderConnection:RBXScriptConnection?,
+	PointA:Vector3?,
+	CurrentPoint:Vector3?,
+	CurrentSnapTarget:Instance?,
+	PointASnapTarget:Instance?,
+	PlacementMode:string,
+	OriginalSize:Vector3?,
+	Hook:(self:BuildClient, targetObject:Instance, mode:string?) -> (),
+	BindInputs:(self:BuildClient) -> (),
+	Place:(self:BuildClient, input:InputObject) -> (),
+	Move:(self:BuildClient) -> (),
+	Rotate:(self:BuildClient, direction:number) -> (),
+	Scale:(self:BuildClient, direction:number) -> (),
+	TryRemove:(self:BuildClient) -> (),
+	Unhook:(self:BuildClient) -> (),
+	Undo:(self:BuildClient) -> (),
+	Redo:(self:BuildClient) -> (),
+	Remove:(self:BuildClient) -> (),
+}
+
 return _Types
